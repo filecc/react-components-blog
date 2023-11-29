ef31c5ec4c448f8960fd61d6b9417b35b100a781
filecc <fil.gia@live.it>
2023-11-29 14:17:07 +0100

    Update Footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return ( <>
         <footer>
-            Footer
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis quibusdam, culpa id sint quisquam minima sit beatae natus eaque. Consequatur atque expedita pariatur assumenda illum, delectus qui adipisci mollitia exercitationem?
         </footer>
     </>)
 }
